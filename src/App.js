0563f90ab00305b5bfa6350ff12391164b8d7075
ghaniz <ghanizahid@hotmail.com>
2022-08-04 21:04:10 -0400

    Activity day 6 - react and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,115 @@
 import './App.css';
+import Nav from 'react-bootstrap/Nav'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Table from 'react-bootstrap/Table';
 
+function BasicExample() {
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="https://images.pexels.com/photos/167699/pexels-photo-167699.jpeg" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+function TableExample() {
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Username</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td colSpan={2}>Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </Table>
+  );
+}
 
 
 function App() {
   return (
     <div className="App">
       <h1>Bootstrap Website</h1>
+
+    <Nav activeKey="/home">
+      <Nav.Item>
+        <Nav.Link href="/home">Home</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="/about">About Us</Nav.Link>
+      </Nav.Item>
+    </Nav>
+   <h2>Form Example</h2>
+    <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+    </Form>
+    <h2>Form Group Example</h2>
+    <Form.Group>
+      <Form.Label>Form Dropdown with favorite movies</Form.Label>
+      <Form.Control as="select">
+        <option>Inception</option>
+        <option>Forrest Gump</option>
+        <option>Shawshank Redemption</option>
+        <option>Spiderman: Into the Spiderverse</option>
+        <option>Avengers: Endgame</option>
+      </Form.Control>
+    </Form.Group>
      
+    <BasicExample />
+    <TableExample />
+
+
+
+
     </div>
   );
 }
